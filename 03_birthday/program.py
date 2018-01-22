a774b85c919b53a81cb027e439027f4ca722b39e
Justin Miller <jmiller9@gmail.com>
2018-01-22 13:16:53 -0500

    working on birthday app

---
+++ 03_birthday/program.py
@@ -0,0 +1,30 @@
+import datetime
+
+def print_header():
+    print('---------------------------')
+    print('       BIRTHDAY APP')
+    print('---------------------------')
+
+def get_birthday_from_user():
+    print("When were you born? ")
+    year = int(input("Year [YYYY]: "))
+    month = int(input("Month [MM]: "))
+    day = int(input("Day [DD]: "))
+
+    birthday = datetime.date(year, month, day)
+    return birthday
+
+def compute_days_between_dates():
+    pass
+
+def print_birthday_information():
+    pass
+
+def main():
+    print_header()
+    bday = get_birthday_from_user()
+    now = None
+    number_of_days = compute_days_between_dates(bday, now)
+    print_birthday_information(number_of_days)
+
+main()
